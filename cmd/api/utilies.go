9ff8cd875bfce6afb66458da7c307f9bdd24eb47
Nawaf Almansour <nawaaf.almansour@gmail.com>
2021-12-01 00:27:55 +0300

    added error hamdling

--- cmd/api/utilies.go
+++ cmd/api/utilies.go
@@ -22,3 +22,15 @@ func (app *application) writeJSON( w http.ResponseWriter, status int, data inter
 	return nil
 
 }
+
+func (app *application) errorJSON( w http.ResponseWriter, err error)  {
+	type jsonError struct {
+		Message string `json:"message"`
+	}
+
+	theError := jsonError{
+		Message: err.Error(),
+	}
+	app.writeJSON(w, http.StatusBadRequest, theError, "error")
+
+}
