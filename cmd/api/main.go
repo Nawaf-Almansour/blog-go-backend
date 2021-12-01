b053e2afe63f4ed7bc87a26b9eeff65a924cd197
Nawaf Almansour <nawaaf.almansour@gmail.com>
2021-12-01 16:02:29 +0300

    added db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,9 @@ const version = "1.0.0"
 type config struct {
 	 port int
 	 env string
+	 db struct {
+		 dsn string
+	 }
 
 }
 type application struct {
@@ -32,9 +39,16 @@ func main()  {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env","development", "Application environment (development | production)")
+	flag.StringVar(&cfg.db.dsn,"dsn","postgres://postgres@localhost/go_movies?sslmode=disable","Postgres connection string")
 	flag.Parse()
 
 	logger := log.New(os.Stderr, "", log.Ldate| log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -48,9 +62,25 @@ func main()  {
 		WriteTimeout: 30 * time.Second,
 	}
 	logger.Println("Starting server on port", cfg.port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error)  {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
