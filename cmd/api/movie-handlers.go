773f091c2003e4c1b7fe3dfc2f5a9ac5840ec82b
Nawaf Almansour <nawaaf.almansour@gmail.com>
2021-12-01 19:07:49 +0300

    added query

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"backend/models"
 	"errors"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
-	"time"
 )
 
 func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
@@ -19,18 +17,19 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	}
 	app.logger.Println("id is ", id)
 
-	movie := models.Movie{
-		ID: id,
-		Title: "some title",
-		Description: "Some description",
-		Year: 2010,
-		ReleaseDate: time.Date(2010,01,01,01,0,0,0,time.Local),
-		Runtime: 100,
-		Rating: 5,
-		MPAARating: "PG-13",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
+	movie, err := app.models.DB.Get(id)
+	//movie := models.Movie{
+	//	ID: id,
+	//	Title: "some title",
+	//	Description: "Some description",
+	//	Year: 2010,
+	//	ReleaseDate: time.Date(2010,01,01,01,0,0,0,time.Local),
+	//	Runtime: 100,
+	//	Rating: 5,
+	//	MPAARating: "PG-13",
+	//	CreatedAt: time.Now(),
+	//	UpdatedAt: time.Now(),
+	//}
 err = app.writeJSON(w, http.StatusOK, movie, "movie")
 }
 
