e545802c740bbbdb675fffc24d5c2ce5882d7592
Nawaf Almansour <nawaaf.almansour@gmail.com>
2021-12-17 23:38:49 +0300

    added genres

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -51,3 +51,16 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 		return
 	}
  }
+ func (app *application) getAllGenres(w http.ResponseWriter, r *http.Request) {
+	 genres, err := app.models.DB.GenresAll()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, genres,"genres")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+ }
